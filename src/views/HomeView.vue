9230e3ad77968fcad87d8e81c1dd3b156bc8cf09
yskms <masashi.yasaka@gmail.com>
2023-01-27 00:23:33 +0900

    無理やり形にしたけどださい

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@ export default {
   // components: { HelloWorld },
   data(){
     return{
-      mv:"works",
+      mv:true,
     }
   },
   mounted(){
@@ -15,9 +15,9 @@ export default {
   methods:{
     navSwitch(e){
       if(e==1){
-        this.mv="works"
+        this.mv=true
       }else if(e==2){
-        this.mv="git"
+        this.mv=false
       }
     }
   },
@@ -34,17 +34,63 @@ export default {
       <div class="header">
         <h1>yskms</h1>
         <nav>
-          <p @click="navSwitch(1)">works</p>
-          <p @click="navSwitch(2)">git</p>
+          <p class="header_ttl" @click="navSwitch(1)" :class="{underline:mv}">works</p>
+          <p class="header_ttl" @click="navSwitch(2)" :class="{underline:!mv}">git</p>
         </nav>
       </div>
       <div class="main">
-        <div class="works_cont" v-if="mv=='works'">
-          <div>works</div>
-          <div>works</div>
-          <div>works</div>
+        <div class="works_cont" v-if="mv==true">
+          <div class="works_main">
+            <div class="works_main_left">
+              <img src="../assets/hakase.png" alt="">
+            </div>
+            <div class="works_main_right">
+              <p>
+                <a>プレゼン資料 : </a>
+                <a href="https://pitchdeck.hypermatic.com/slides/l9rx5yyj40410?token=VjR4biQlU2lIYmhIelo%3D">pitchdeck</a>
+              </p>
+              <p>
+                <a>Netlify : </a>
+                <a href="https://remarkable-salmiakki-887cba.netlify.app/">app</a>
+              </p>
+              <p>
+                <a>Github : </a>
+                <a href="https://github.com/yskms/1023_flag_app_v2">URL</a>
+              </p>
+            </div>
+          </div>
+          <div class="works_main">
+            <div class="works_main_left">
+              <img src="../assets/rikare.svg" alt="">
+            </div>
+            <div class="works_main_right">
+              <p>
+                <a>Netlify : </a>
+                <a href="https://deft-toffee-382a3f.netlify.app/">app</a>
+              </p>
+              <p>
+                <a>Github : </a>
+                <a href="https://github.com/yskms/1117_rikameshi.git">URL</a>
+              </p>
+            </div>
+          </div>
+          <div class="works_main">
+            <div class="works_main_left">
+              <img src="../assets/utc.svg" alt="">
+            </div>
+            <div class="works_main_right">
+              <p>
+                <a>Netlify : </a>
+                <a href="https://chic-lebkuchen-daab8b.netlify.app/">app</a>
+              </p>
+              <p>
+                <a>Github : </a>
+                <a href="https://github.com/yskms/1205_utc_now_v3.git">URL</a>
+              </p>
+            </div>
+          </div>
         </div>
-        <div class="git_cont" v-else-if="mv=='git'">
+        <div class="git_cont" v-else-if="mv==false">
           <p>git</p>
           <a href="https://github.com/yskms">https://github.com/yskms</a>
         </div>
@@ -55,7 +101,7 @@ export default {
 
 <style scoped>
 .bg{
-  background-color: blueviolet;
+  background-color: black;
   width: 100vw;
   height: 100vh;
 }
@@ -100,6 +146,7 @@ export default {
   opacity:0;
   color: #ffffff;
   width: 100%;
+  height: 15%;
 }
 @keyframes headerAnime{
   0% {opacity: 0;  }
@@ -115,6 +162,15 @@ export default {
   display: flex;
   justify-content: space-around;
 }
+.header_ttl{
+  transition: 0.5s;
+}
+.header_ttl:hover{
+  transform: scale(1.2);
+}
+.underline{
+  border-bottom: solid white 1px;
+}
 /* ------------------------ */
 .main{
   animation-name:headerAnime;
@@ -122,9 +178,32 @@ export default {
   animation-fill-mode: forwards;
   opacity:0;
   color: #ffffff;
+  height: 85%;
 }
 .works_cont{
-  display: grid;
-  grid-template-columns: 1fr 1fr ;
+  height: 100%;
+  /* display: grid; */
+  /* grid-template-columns: 1fr 1fr ; */
+}
+.works_main{
+  display: flex;
+  height: 20%;
+  width: 90%;
+  margin: 0 auto;
+  padding: 1em;
+}
+.works_main_left{
+  width: 30%;
+}
+.works_main_left img{
+  height: 100%;
+}
+.works_main_right{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-left: 1em;
+  width: 70%;
 }
 </style>
